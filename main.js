d0a2bd7843b98af03c9e99fcd8b2bbf3bd377ee4
彥翰 李 <yanhanli@yanhandeMacBook-Pro.local>
2016-06-05 11:54:58 +0800

    hihi

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 if(Meteor.isClient){
 	Template.body.helpers({
 		messages:[
-			{text :'text1' , num : 1 , num2 : 3},
-			{text :'text2' , num : 2},
-			{text :'text3' , num : 3}
+			{text :'text1' , num : 1 ,time : '20160605 12:00:00'},
+			{text :'text2' , num : 2 ,time : '20160605 10:00:00'},
+			{text :'text3' , num : 4 ,time : '20160605 08:00:00'}
 		]
 	})
 }
+
