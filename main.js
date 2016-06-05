af5cb20aac6877b4adb98f1c3f1ca0f9dba4913a
彥翰 李 <yanhanli@yanhandeMacBook-Pro.local>
2016-06-05 15:14:11 +0800

    sort last 5 record

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
 Messages = new Mongo.Collection('messages')
 
 sampleMessage = [
-			{text :'text1' , num : 1 ,time : '20160605 12:00:00', from :'db'},
-			{text :'text2' , num : 2 ,time : '20160605 10:00:00', from :'db'},
-			{text :'text3' , num : 4 ,time : '20160605 08:00:00', from :'db'}
+			{text :'text1' , num : 1 ,createdAt : '20160605 12:00:00', from :'jsInput'},
+			{text :'text2' , num : 2 ,createdAt : '20160605 10:00:00', from :'jsInput'},
+			{text :'text3' , num : 4 ,createdAt : '20160605 08:00:00', from :'jsInput'}
 ]
 
 if(Meteor.isClient){
 	Template.body.helpers({
 		messages:function(){
-			return Messages.find()
+			return Messages.find({}, { sort : { createdAt : -1} ,limit : 5})
 		}
 	})
+
+	Template.body.events({
+		'change #messages': function(event){
+			let text = $(event.target).val()
+			let num = Messages.find().count()+1
+			$(event.target).val('')
+			let messages ={
+				text,
+				num,
+				from:'db',
+				createdAt: new Date()
+			}
+			Messages.insert(messages)
+		}
+		
+	})
+
 }
 
 
