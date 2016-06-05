dfb13a1f04140884cd5054d55f2471d3e98332a0
彥翰 李 <yanhanli@yanhandeMacBook-Pro.local>
2016-06-05 14:17:54 +0800

    hihi

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
+Messages = new Mongo.Collection('messages')
+
+sampleMessage = [
+			{text :'text1' , num : 1 ,time : '20160605 12:00:00', from :'db'},
+			{text :'text2' , num : 2 ,time : '20160605 10:00:00', from :'db'},
+			{text :'text3' , num : 4 ,time : '20160605 08:00:00', from :'db'}
+]
+
 if(Meteor.isClient){
 	Template.body.helpers({
-		messages:[
-			{text :'text1' , num : 1 ,time : '20160605 12:00:00'},
-			{text :'text2' , num : 2 ,time : '20160605 10:00:00'},
-			{text :'text3' , num : 4 ,time : '20160605 08:00:00'}
-		]
+		messages:function(){
+			return Messages.find()
+		}
 	})
 }
 
+
+if(Meteor.isServer){
+	if(Messages.find().count()===0){
+		for(let messages of sampleMessage){
+			Messages.insert(messages)
+		}
+	}
+}
